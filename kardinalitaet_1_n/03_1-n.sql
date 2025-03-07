2848d4a119210b443b366fba9cdf43317f1d3333
jessica <jessica.walker93@hotmail.it>
2025-03-07 12:48:18 +0100

    KITTENS

--- kardinalitaet_1_n/03_1-n.sql
+++ kardinalitaet_1_n/03_1-n.sql
@@ -18,23 +18,43 @@ DESCRIBE design.catmoms;
 
 -- MT: Inserts
 INSERT INTO design.catmoms (id, cat_name, fur_color) VALUES
-(DEFAULT, "Moon", "white"),
-(DEFAULT, "Pettimo", "orange"),
-(DEFAULT, "Shadow", "black"),
-(DEFAULT, "Oldie", "grey"),
-(DEFAULT, "Tiger", "striped")
+(DEFAULT, "Marshmallow", "white"),
+(DEFAULT, "Cinnamon", "orange"),
+(DEFAULT, "Donut", "black"),
+(DEFAULT, "Ashy", "grey"),
+(DEFAULT, "Waffle", "striped")
 ;
 -- MT: Inhalte
 SELECT*FROM design.catmoms;
 
 -- DT: kittens
+CREATE TABLE IF NOT EXISTS design.kittens
+(
+  id          INT         NOT NULL AUTO_INCREMENT,
+  kitten_name VARCHAR(45) NOT NULL,
+  fur_color   VARCHAR(45) NOT NULL,
+  catmoms_id  INT         NOT NULL,
+  PRIMARY KEY (id)
+);
 
+ALTER TABLE design.kittens
+  ADD CONSTRAINT FK_catmoms_TO_kittens
+    FOREIGN KEY (catmoms_id)
+    REFERENCES catmoms (id);
 
 -- DT: Struktur
-
+DESCRIBE design.kittens;
 
 -- DT: Inserts
-
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES (DEFAULT, "Gumdrop", "white", 1),
+(DEFAULT, "Candy", "white", 1),
+(DEFAULT, "Toffe", "white", 1)
+;
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES (DEFAULT, "Cotton", "white", 1);
+INSERT INTO design.kittens (id, kitten_name, fur_color, catmoms_id) VALUES (DEFAULT, "Sprinkles", "striped", 2);
 
 -- DT: Inhalte
+SELECT * FROM design.kittens;
+
+
 
